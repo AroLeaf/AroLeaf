af50d569804f0e35eca258af40805779d06055e5
AroLeaf <starleafofa@gmail.com>
2022-10-28 11:50:29 +0200

    [feat] update everything

--- gallery.js
+++ gallery.js
@@ -1,24 +1,29 @@
 class Gallery {
   constructor(query) {
-    this.elements = Array.from(document.querySelectorAll(query));
-    this.index = 0;
-    this.set(0);
+    if (query) this.update(query);
   }
 
   get current() {
     return this.elements[this.index];
   }
-
+  
   set(idx) {
     this.current?.classList.remove('active');
-    this.index = (this.elements.length + idx) % this.elements.length;
+    this.index = this.elements.length && (this.elements.length + idx) % this.elements.length;
     this.current?.classList.add('active');
   }
-
+  
   left() {
     this.set(this.index - 1);
   }
   right() {
     this.set(this.index + 1);
+  }
+
+  update(query) {
+    if (query) this.query = query;
+    this.elements = Array.from(document.querySelectorAll(this.query));
+    this.index = 0;
+    this.set(0);
   }
 }
