10fb383c4808bd924dcdca97c9828dfabe6b6edf
Leaf <starleafofa@gmail.com>
2024-03-17 19:23:43 +0100

    [feat] add blog [feat] add blogpost about how I made this site

---
+++ .pug.mjs
@@ -0,0 +1,34 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import pug from 'pug';
+import hljs from 'highlight.js';
+
+export function include(fp, locals = {}) {
+  const content = fs.readFileSync(fp, 'utf-8');
+  return path.extname(fp) === '.pug' ? pug.render(content, {
+    ...locals,
+    basedir: path.dirname(fp),
+    filename: fp,
+  }) : content;
+}
+
+export const filters = {
+  highlight(text, options) {
+    if (typeof options.language !== 'string') return hljs.highlightAuto(text).value;
+    return hljs.highlight(text, { language: options.language, ignoreIllegals: true }).value;
+  },
+
+  prefixCSS(text, options) {
+    return text.replace(/(?<=}|^).*?(?={)/g, `${options.prefix} :is($&)`);
+  },
+}
+
+
+export const blogposts = [
+  {
+    id: 'you-probably-dont-need-javascript',
+    title: 'You probably don\'t need Javascript.',
+    description: 'Or maybe you do, idk tbh',
+    timestamp: new Date().toISOString(),
+  },
+].sort((a, b) => a.timestamp > b.timestamp ? -1 : a.timestamp < b.timestamp ? 1 : 0);
